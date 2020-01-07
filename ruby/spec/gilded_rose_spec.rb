48d9c0cb154dd1b11b7c27dda0217485516d3ec6
zraso <zaira.rasool@hotmail.co.uk>
2020-01-07 14:44:01 +0000

    Tests completed

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -3,7 +3,16 @@ require 'gilded_rose'
 describe GildedRose do
 
   before :each do
-    @items=[Item.new("foo", 0, 0), Item.new("foo", 0, 6), Item.new("Aged Brie", 10, 6), Item.new("Aged Brie", 10, 50)]
+    @items=[Item.new("foo", 0, 0), 
+            Item.new("foo", 0, 6), 
+            Item.new("Aged Brie", 10, 6), 
+            Item.new("Aged Brie", 10, 50),
+            Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+            Item.new(name="Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Item.new(name="Backstage passes to a TAFKAL80ETC concert", 9, 20),
+            Item.new(name="Backstage passes to a TAFKAL80ETC concert", 3, 20),
+            Item.new(name="Backstage passes to a TAFKAL80ETC concert", 0, 0),
+            Item.new(name="Conjured Mana Cake", 3, 6)]
     GildedRose.new(@items).update_quality
   end
 
@@ -34,6 +43,40 @@ describe GildedRose do
     it "never increases the quality above 50" do
       expect(@items[3].quality).to eq 50
     end
+
+    context "when the item is Sulfuras" do
+      it "never changes the sell in date" do
+        expect(@items[4].sell_in).to eq 0
+      end
+
+      it "never decreases in quality" do
+        expect(@items[4].quality).to eq 80
+      end
+    end
+
+    context "when the item is Backstage passes" do
+      it "increases in quality as its SellIn date approaches" do
+        expect(@items[5].quality).to eq 21
+      end
+
+      it "increases in quality by 2 if SellIn is less than 10" do
+        expect(@items[6].quality).to eq 22
+      end
+
+      it "increases in quality by 3 if SellIn is less than 5" do
+        expect(@items[7].quality).to eq 23
+      end
+
+      it "decreases quality to zero after the concert" do
+        expect(@items[8].quality).to eq 0
+      end
+    end
+
+    # context "when the item is Conjured" do
+    #   it "degrades in quality by 2" do
+    #     expect(@items[9].quality).to eq 4
+    #   end
+    # end
   end
 
 end
