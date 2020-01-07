94597d5887f4c0d98c54f83c5f4010605151c0e5
zraso <zaira.rasool@hotmail.co.uk>
2020-01-07 14:49:16 +0000

    Adds increment operators

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -9,7 +9,7 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            item.quality -= 1
           end
         end
       else
@@ -18,34 +18,34 @@ class GildedRose
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
           end
         end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
       end
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                item.quality -= 1
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality -= item.quality
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            item.quality += 1
           end
         end
       end
