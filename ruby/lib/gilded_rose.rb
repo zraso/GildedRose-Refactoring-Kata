2eb4efc4534c0b0786a167bdb90875d2e35076e6
zraso <zaira.rasool@hotmail.co.uk>
2020-01-08 17:04:06 +0000

    refactors normal items logic

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,65 +1,48 @@
-class GildedRose
+# frozen_string_literal: true
 
+class GildedRose
   def initialize(items)
     @items = items
   end
 
   def update_quality
     @items.each do |item|
-
       update_sell_in(item)
 
       update_brie_quality(item)
 
-      if normal?(item)
-
-        if item.quality > 0 && item.sell_in < 0
-          item.quality -= 2
-        elsif item.quality > 0
-          item.quality -= 1
-        end
-      end
+      update_normal_quality(item) if normal?(item)
 
       if backstage?(item)
-
         item.quality += 1
         item.quality += 1 if item.sell_in < 11
         item.quality += 1 if item.sell_in < 6
         item.quality -= item.quality if item.sell_in < 0
-            
-        # if item.sell_in < 11
-        #   item.quality += 1
-        # end
-
-        # if item.sell_in < 6
-        #   item.quality += 1
-        # end
-
-        # if item.sell_in < 0
-        #   item.quality -= item.quality
-        # end
-            
       end
     end
   end
 
   def update_sell_in(item)
-    item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
+    item.sell_in -= 1 unless item.name == 'Sulfuras, Hand of Ragnaros'
   end
 
-  ##The item.quality < 50 bit is hacky. We need a method that checks if quality is under 50 and only updates if so
+  # #The item.quality < 50 bit is hacky. We need a method that checks if quality is under 50 and only updates if so
   def update_brie_quality(item)
-    item.quality += 1 if (item.name == "Aged Brie" && item.quality < 50)
+    item.quality += 1 if item.name == 'Aged Brie' && item.quality < 50
+  end
+
+  def update_normal_quality(item)
+    item.quality -= 1 if item.quality > 0
+    item.quality -= 1 if item.quality > 0 && item.sell_in < 0
   end
 
   def normal?(item)
-    item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros"
+    item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros'
   end
 
   def backstage?(item)
-    item.name == "Backstage passes to a TAFKAL80ETC concert"
+    item.name == 'Backstage passes to a TAFKAL80ETC concert'
   end
-
 end
 
 class Item
@@ -71,7 +54,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,73 +1,73 @@
+# frozen_string_literal: true
+
 require 'gilded_rose'
 
 describe GildedRose do
-
   before :each do
-    @items=[Item.new("foo", 0, 0), 
-            Item.new("foo", 0, 6), 
-            Item.new("Aged Brie", 10, 6), 
-            Item.new("Aged Brie", 10, 50),
-            Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
-            Item.new(name="Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            Item.new(name="Backstage passes to a TAFKAL80ETC concert", 9, 20),
-            Item.new(name="Backstage passes to a TAFKAL80ETC concert", 3, 20),
-            Item.new(name="Backstage passes to a TAFKAL80ETC concert", 0, 0),
-            Item.new(name="Conjured Mana Cake", 3, 6)]
+    @items = [Item.new('foo', 0, 0),
+              Item.new('foo', 0, 6),
+              Item.new('Aged Brie', 10, 6),
+              Item.new('Aged Brie', 10, 50),
+              Item.new('Sulfuras, Hand of Ragnaros', 0, 80),
+              Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', 15, 20),
+              Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', 9, 20),
+              Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', 3, 20),
+              Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', 0, 0),
+              Item.new(name = 'Conjured Mana Cake', 3, 6)]
     GildedRose.new(@items).update_quality
   end
 
-  describe "#update_quality" do
-
+  describe '#update_quality' do
     context 'when the sell by date has passed' do
-      it "degrades quality twice as fast" do
+      it 'degrades quality twice as fast' do
         expect(@items[1].quality).to eq 4
       end
     end
 
-    context "when sell by date has passed and quality is at zero" do
-      it "does not change the name" do
-        expect(@items[0].name).to eq "foo"
+    context 'when sell by date has passed and quality is at zero' do
+      it 'does not change the name' do
+        expect(@items[0].name).to eq 'foo'
       end
 
-      it "does not change quality of an item to negative" do
+      it 'does not change quality of an item to negative' do
         expect(@items[0].quality).to eq 0
       end
     end
 
-    context "when the item is Aged Brie" do
-      it "increases in quality the older it gets" do
+    context 'when the item is Aged Brie' do
+      it 'increases in quality the older it gets' do
         expect(@items[2].quality).to eq 7
       end
     end
 
-    it "never increases the quality above 50" do
+    it 'never increases the quality above 50' do
       expect(@items[3].quality).to eq 50
     end
 
-    context "when the item is Sulfuras" do
-      it "never changes the sell in date" do
+    context 'when the item is Sulfuras' do
+      it 'never changes the sell in date' do
         expect(@items[4].sell_in).to eq 0
       end
 
-      it "never decreases in quality" do
+      it 'never decreases in quality' do
         expect(@items[4].quality).to eq 80
       end
     end
 
-    context "when the item is Backstage passes" do
-      it "increases in quality as its SellIn date approaches" do
+    context 'when the item is Backstage passes' do
+      it 'increases in quality as its SellIn date approaches' do
         expect(@items[5].quality).to eq 21
       end
 
-      it "increases in quality by 2 if SellIn is less than 10" do
+      it 'increases in quality by 2 if SellIn is less than 10' do
         expect(@items[6].quality).to eq 22
       end
 
-      it "increases in quality by 3 if SellIn is less than 5" do
+      it 'increases in quality by 3 if SellIn is less than 5' do
         expect(@items[7].quality).to eq 23
       end
 
-      it "decreases quality to zero after the concert" do
+      it 'decreases quality to zero after the concert' do
         expect(@items[8].quality).to eq 0
       end
     end
@@ -78,5 +78,4 @@ describe GildedRose do
     #   end
     # end
   end
-
 end
